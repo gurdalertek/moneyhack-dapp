17a5352a03febc4f98750d40bbec899cce3fefd9
Gunvant Kathrotiya <gunvantk@users.noreply.github.com>
2022-05-10 22:24:30 -0700

    Added events and modifiers to factory contract

--- packages/hardhat/contracts/RBFVaultFactory.sol
+++ packages/hardhat/contracts/RBFVaultFactory.sol
@@ -1,21 +1,68 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol";
 import "./RBFVault.sol";
 
 contract RBFVaultFactory {
+    event RBFVaultCreated(
+        address indexed collectionAddress,
+        address indexed vaultAddress
+    );
+
     mapping(address => address) public collectionVault;
+    modifier collectionVaultDoesntExist(address _collectionAddress) {
+        require(
+            _collectionAddress == address(0),
+            "Vault for this collection already exist"
+        );
+        _;
+    }
+
+    modifier sharesIsValid(uint256 share) {
+        require(share <= 100, "Investor shares can not be more than 100");
+        require(share > 0, "Investor shares should be more than 0");
+        _;
+    }
+
+    modifier isValidAddress(
+        address _collectionAddress,
+        address _collectionOwner,
+        address _investorAddress
+    ) {
+        require(
+            _collectionAddress != address(0),
+            "Collection can not be the 0 address"
+        );
+        require(
+            _collectionOwner != address(0),
+            "Collection owner can not be the 0 address"
+        );
+
+        require(
+            _investorAddress != address(0),
+            "Investor can not be the 0 address"
+        );
+
+        require(
+            _collectionOwner != _investorAddress,
+            "Collection owner can not be the investor"
+        );
+
+        _;
+    }
 
     function createVault(
         address _collectionAddress,
         address _collectionOwner,
         address _investorAddress,
-        int256 _investorShare
-    ) external onlyowner {
+        uint256 _investorShare
+    ) external collectionVaultDoesntExist isValidAddress sharesIsValid {
         address[2] memory parties = [_investorAddress, _collectionOwner];
-        int256[2] memory shares = [_investorShare, 100 - _investorShare];
+        uint256[2] memory shares = [_investorShare, 100 - _investorShare];
         RBFVault vault = new RBFVault(_collectionAddress, parties, shares);
         collectionVault[_collectionAddress] = address(vault);
+
+        emit RBFVaultCreated(_collectionAddress, address(vault));
     }
 }
